d321a2a35bbe2ec5f1a7a3c77f9a8d57403e90bb
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-08-25 23:31:43 -0300

    feat: add list project

--- src/application/usecases/project/list-project.usecase.ts
+++ src/application/usecases/project/list-project.usecase.ts
@@ -1,27 +1,14 @@
 import { ProjectRepository } from '@application/repositories'
-import { Project } from '@domain/entities'
 import { inject, injectable } from 'tsyringe'
 
-type ProjectProps = {
-  name: string
-}
-
 @injectable()
-export class CreateProjectUseCase {
+export class ListProjectUseCase {
   constructor (
     @inject('InMemoryProjectRepository')
     private projectRepository: ProjectRepository
   ) {}
 
-  async execute ({ name }: ProjectProps) {
-    const projectExists = await this.projectRepository.findByName(name)
-
-    if (projectExists) {
-      throw new Error('Project already exists')
-    }
-
-    const project = Project.create({ name })
-
-    return await this.projectRepository.create(project)
+  async execute () {
+    return await this.projectRepository.list()
   }
 }
