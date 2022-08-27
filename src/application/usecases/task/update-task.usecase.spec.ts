2266534b45b8b7718398717b4ce2bba240a575de
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-08-27 10:34:05 -0300

    test: add projectId

--- src/application/usecases/task/update-task.usecase.spec.ts
+++ src/application/usecases/task/update-task.usecase.spec.ts
@@ -1,15 +1,20 @@
-import { InMemoryTaskRepository } from '@tests/repositories'
+import { InMemoryProjectRepository, InMemoryTaskRepository } from '@tests/repositories'
 import { UpdateTaskUseCase, CreateTaskUseCase } from '.'
+import { CreateProjectUseCase } from '../project'
 
 let taskRepository: InMemoryTaskRepository
+let projectRepository: InMemoryProjectRepository
+let createProjectUseCase: CreateProjectUseCase
 let createTaskUseCase: CreateTaskUseCase
 let sut: UpdateTaskUseCase
 
 describe('UpdateTaskUseCase', () => {
   beforeEach(() => {
     taskRepository = new InMemoryTaskRepository()
-    createTaskUseCase = new CreateTaskUseCase(taskRepository)
-    sut = new UpdateTaskUseCase(taskRepository)
+    projectRepository = new InMemoryProjectRepository()
+    createTaskUseCase = new CreateTaskUseCase(taskRepository, projectRepository)
+    createProjectUseCase = new CreateProjectUseCase(projectRepository)
+    sut = new UpdateTaskUseCase(taskRepository, projectRepository)
   })
 
   it('should throw if task is not found', async () => {
@@ -18,30 +23,60 @@ describe('UpdateTaskUseCase', () => {
       name: 'any_name',
       responsible: 'any_responsible',
       status: 'any_status',
-      finishDate: new Date()
+      finishDate: new Date(),
+      projectId: 'any_project_id'
+    })
+
+    await expect(promise).rejects.toThrow()
+  })
+
+  it('should throw if project is not found', async () => {
+    const task = await createTaskUseCase.execute({
+      name: 'task',
+      responsible: 'task responsible',
+      status: 'pendente',
+      finishDate: new Date(),
+      projectId: 'any_project_id'
+    })
+
+    const promise = sut.execute({
+      id: task.id,
+      name: 'any_name',
+      responsible: 'any_responsible',
+      status: 'any_status',
+      finishDate: new Date(),
+      projectId: 'invalid_id'
     })
 
     await expect(promise).rejects.toThrow()
   })
 
   it('should update task', async () => {
-    const { id } = await createTaskUseCase.execute({
-      name: 'any_name',
-      responsible: 'any_responsible',
-      status: 'any_status',
-      finishDate: new Date()
+    const project = await createProjectUseCase.execute({
+      name: 'project'
     })
 
-    const task = await sut.execute({
-      id,
-      name: 'new_name',
-      responsible: 'new_responsible',
-      status: 'new_status',
-      finishDate: new Date()
+    const task = {
+      name: 'task',
+      responsible: 'task responsible',
+      status: 'pendente',
+      finishDate: new Date(),
+      projectId: project.id
+    }
+
+    const createdTask = await createTaskUseCase.execute(task)
+
+    const updatedTask = await sut.execute({
+      id: createdTask.id,
+      name: 'updated task',
+      responsible: 'updated task responsible',
+      status: 'updated status',
+      finishDate: new Date(),
+      projectId: project.id
     })
 
-    expect(task.name).toBe('new_name')
-    expect(task.responsible).toBe('new_responsible')
-    expect(task.status).toBe('new_status')
+    expect(updatedTask.name).toBe('updated task')
+    expect(updatedTask.responsible).toBe('updated task responsible')
+    expect(updatedTask.status).toBe('updated status')
   })
 })
