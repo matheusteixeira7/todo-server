a488da21a22fb9637608e50d38be7e7ef6b99493
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-08-27 10:33:51 -0300

    test: add projectId

--- src/application/usecases/task/create-task.usecase.spec.ts
+++ src/application/usecases/task/create-task.usecase.spec.ts
@@ -1,14 +1,19 @@
-import { TaskRepository } from '@application/repositories'
-import { InMemoryTaskRepository } from '@tests/repositories'
+import { ProjectRepository, TaskRepository } from '@application/repositories'
+import { InMemoryProjectRepository, InMemoryTaskRepository } from '@tests/repositories'
+import { CreateProjectUseCase } from '../project'
 import { CreateTaskUseCase } from './'
 
 describe('CreateTaskUseCase', () => {
-  let createTaskRepository: TaskRepository
+  let taskRepository: TaskRepository
+  let projectRepository: ProjectRepository
+  let createProjectUseCase: CreateProjectUseCase
   let sut: CreateTaskUseCase
 
   beforeEach(() => {
-    createTaskRepository = new InMemoryTaskRepository()
-    sut = new CreateTaskUseCase(createTaskRepository)
+    taskRepository = new InMemoryTaskRepository()
+    projectRepository = new InMemoryProjectRepository()
+    createProjectUseCase = new CreateProjectUseCase(projectRepository)
+    sut = new CreateTaskUseCase(taskRepository, projectRepository)
   })
 
   it('should NOT be able to create a task with the same name', async () => {
@@ -16,12 +21,45 @@ describe('CreateTaskUseCase', () => {
       name: 'task',
       responsible: 'task responsible',
       status: 'pendente',
-      finishDate: new Date()
+      finishDate: new Date(),
+      projectId: 'project id'
     }
 
     expect(async () => {
       await sut.execute(task)
       await sut.execute(task)
     }).rejects.toThrow()
+  })
+
+  it('should NOT be able to create a task with a non-existent project', async () => {
+    const task = {
+      name: 'task',
+      responsible: 'task responsible',
+      status: 'pendente',
+      finishDate: new Date(),
+      projectId: 'invalid_id'
+    }
+
+    expect(async () => {
+      await sut.execute(task)
+    }).rejects.toThrow()
+  })
+
+  it('should be able to create a task', async () => {
+    const project = await createProjectUseCase.execute({
+      name: 'project'
+    })
+
+    const task = {
+      name: 'task',
+      responsible: 'task responsible',
+      status: 'pendente',
+      finishDate: new Date(),
+      projectId: project.id
+    }
+
+    const createdTask = await sut.execute(task)
+
+    expect(createdTask).toHaveProperty('id')
   })
 })
