86c0b8382ea57bb5c83ce7ebbd785db7ee55f381
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-08-27 00:00:42 -0300

    feat: update import

--- src/application/usecases/task/index.ts
+++ src/application/usecases/task/index.ts
@@ -1,3 +1,5 @@
 export * from './create-task.usecase'
-export * from './update-task.usecase'
+export * from './delete-task.usecase'
+export * from './get-task.usecase'
 export * from './list-task.usecase'
+export * from './update-task.usecase'
