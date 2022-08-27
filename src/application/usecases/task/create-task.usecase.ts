b37476e2b5ec9d551ddc268f9e0e564163400fc2
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-08-26 23:35:55 -0300

    feat: add create task

---
+++ src/application/usecases/task/create-task.usecase.ts
@@ -0,0 +1,30 @@
+import { TaskRepository } from '@application/repositories'
+import { Task } from '@domain/entities'
+import { inject, injectable } from 'tsyringe'
+
+type TaskProps = {
+  name: string
+  responsible: string
+  status: string
+  finishDate: Date
+}
+
+@injectable()
+export class CreateTaskUseCase {
+  constructor (
+    @inject('InMemoryTaskRepository')
+    private taskRepository: TaskRepository
+  ) {}
+
+  async execute ({ name, responsible, status, finishDate }: TaskProps) {
+    const taskExists = await this.taskRepository.findByName(name)
+
+    if (taskExists) {
+      throw new Error('Project already exists')
+    }
+
+    const task = Task.create({ name, responsible, status, finishDate })
+
+    return await this.taskRepository.create(task)
+  }
+}
--- tests/repositories/in-memory-task.repository.ts
+++ tests/repositories/in-memory-task.repository.ts
@@ -4,31 +4,61 @@ import { Task } from '@domain/entities'
 export class InMemoryTaskRepository implements TaskRepository {
   public items: Task[] = []
 
-  findById (id: string): Promise<Task | null> {
-    throw new Error('Method not implemented.')
+  async findById (id: string): Promise<Task | null> {
+    const task = this.items.find(t => t.id === id)
+
+    if (!task) {
+      return null
+    }
+
+    return task
   }
 
-  findByIds (ids: string[]): Promise<Task[]> {
-    throw new Error('Method not implemented.')
+  async findByIds (ids: string[]): Promise<Task[]> {
+    return this.items.filter(t => ids.includes(t.id))
   }
 
-  findByName (name: string): Promise<Task | null> {
-    throw new Error('Method not implemented.')
+  async findByName (name: string): Promise<Task | null> {
+    const task = this.items.find(t => t.name === name)
+
+    if (!task) {
+      return null
+    }
+
+    return task
   }
 
-  create (task: Task): Promise<Task> {
-    throw new Error('Method not implemented.')
+  async create (task: Task): Promise<Task> {
+    const findIndex = this.items.findIndex(t => t.id === task.id)
+
+    if (findIndex === -1) {
+      this.items.push(task)
+    }
+
+    return task
   }
 
-  update (task: Task): Promise<Task> {
-    throw new Error('Method not implemented.')
+  async update (task: Task): Promise<Task> {
+    const findIndex = this.items.findIndex(t => t.id === task.id)
+
+    if (findIndex !== -1) {
+      this.items[findIndex] = task
+    }
+
+    return task
   }
 
-  list (): Promise<Task[]> {
-    throw new Error('Method not implemented.')
+  async list (): Promise<Task[]> {
+    return this.items
   }
 
-  delete (id: string): Promise<void> {
-    throw new Error('Method not implemented.')
+  async delete (id: string): Promise<void> {
+    const index = this.items.findIndex(t => t.id === id)
+
+    if (index === -1) {
+      throw new Error('Service not found.')
+    }
+
+    this.items.splice(index, 1)
   }
 }
