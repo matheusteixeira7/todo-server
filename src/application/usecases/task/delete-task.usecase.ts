1acf2295ad0d5df28b443895fed67c7671321710
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-08-27 00:09:41 -0300

    feat: add delete task

---
+++ src/application/usecases/task/delete-task.usecase.ts
@@ -0,0 +1,24 @@
+import { TaskRepository } from '@application/repositories'
+import { inject, injectable } from 'tsyringe'
+
+type TaskProps = {
+  id: string
+}
+
+@injectable()
+export class DeleteTaskUseCase {
+  constructor (
+    @inject('InMemoryTaskRepository')
+    private taskRepository: TaskRepository
+  ) {}
+
+  async execute ({ id }: TaskProps) {
+    const task = await this.taskRepository.findById(id)
+
+    if (!task) {
+      throw new Error('Task not found')
+    }
+
+    await this.taskRepository.delete(task.id)
+  }
+}
