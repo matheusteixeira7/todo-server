d283cdf6fa496a408982bc75f63fd5d4c2431961
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-08-27 10:33:55 -0300

    feat: add projectId

--- src/application/usecases/task/create-task.usecase.ts
+++ src/application/usecases/task/create-task.usecase.ts
@@ -1,4 +1,4 @@
-import { TaskRepository } from '@application/repositories'
+import { ProjectRepository, TaskRepository } from '@application/repositories'
 import { Task } from '@domain/entities'
 import { inject, injectable } from 'tsyringe'
 
@@ -7,23 +7,32 @@ type TaskProps = {
   responsible: string
   status: string
   finishDate: Date
+  projectId: string
 }
 
 @injectable()
 export class CreateTaskUseCase {
   constructor (
     @inject('InMemoryTaskRepository')
-    private taskRepository: TaskRepository
+    private taskRepository: TaskRepository,
+    @inject('InMemoryProjectRepository')
+    private projectRepository: ProjectRepository
   ) {}
 
-  async execute ({ name, responsible, status, finishDate }: TaskProps) {
+  async execute ({ name, responsible, status, finishDate, projectId }: TaskProps) {
     const taskExists = await this.taskRepository.findByName(name)
 
     if (taskExists) {
       throw new Error('Task already exists')
     }
 
-    const task = Task.create({ name, responsible, status, finishDate })
+    const project = await this.projectRepository.findById(projectId)
+
+    if (!project) {
+      throw new Error('Project not found')
+    }
+
+    const task = Task.create({ name, responsible, status, finishDate, projectId })
 
     return await this.taskRepository.create(task)
   }
